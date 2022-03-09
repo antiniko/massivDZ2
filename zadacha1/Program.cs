7c00cae81e1917e8b52e0423eb9a9ce37d5a9ef1
antiniko <anti.niko@ya.ru>
2022-03-09 22:18:33 +0300

    1 zadacha done

---
+++ zadacha1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+int rev(int x)
+{
+    if (x > 9999 && x < 100000)
+    {
+    int a1 = x % 10;
+    int a2 = x / 10 % 10;
+    int a3 = x / 100 % 10;
+    int a4 = x / 1000 % 10;
+    int a5 = x / 10000;
+    return  a1 * 10000 + a2 * 1000 + a3 * 100 + a4 * 10 + a5;
+    }
+    else
+    {
+    Console.WriteLine("Не верное число");
+    return 0;
+    ;
+    }
+}
+int chislo;
+Console.Write("введите пятизначное число: ");
+chislo = Convert.ToInt32(Console.ReadLine());
+rev(chislo);
+if (chislo == rev(chislo))
+{
+    Console.WriteLine("чилсло является палиндромом");
+}
+else
+{
+    Console.WriteLine("чилсло не является палиндромом");
+}
+
+
