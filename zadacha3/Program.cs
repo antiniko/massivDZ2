fd82357086f61d6967ab5de4ba3845b4b47d7b20
antiniko <anti.niko@ya.ru>
2022-03-10 00:42:13 +0300

    zadacha3 complete

---
+++ zadacha3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+int chislo;
+Console.Write("введите число: ");
+chislo = Convert.ToInt32(Console.ReadLine());
+int n = 0;
+while(chislo >= n)
+{
+ Console.Write(Math.Pow(n,3));
+ Console.Write("");
+ n++;
+}
+
+
+
+
