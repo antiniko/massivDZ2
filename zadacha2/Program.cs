da3c6c978f1dc1bfe3b16e30893158b3bd927202
antiniko <anti.niko@ya.ru>
2022-03-09 23:51:29 +0300

    zadacha2 complete

---
+++ zadacha2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double GetDistance(double x1, double y1, double z1, double x2,double y2, double z2)
+{
+    double xDistance = x2 - x1;
+    double yDistance = y2 - y1;
+    double zDistance = z2 - z1;
+
+    return Math.Sqrt(Math.Pow(xDistance,2) + Math.Pow(yDistance,2) + Math.Pow(yDistance,2));
+}
+
+double xA, yA, zA, xB, yB, zB;
+
+Console.Write("input x for point A: ");
+xA = Convert.ToDouble(Console.ReadLine());
+Console.Write("input y for point A: ");
+yA = Convert.ToDouble(Console.ReadLine());
+Console.Write("input z for point A: ");
+zA = Convert.ToDouble(Console.ReadLine());
+Console.Write("input x for point B: ");
+xB = Convert.ToDouble(Console.ReadLine());
+Console.Write("input y for point B: ");
+yB = Convert.ToDouble(Console.ReadLine());
+Console.Write("input z for point B: ");
+zB = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(GetDistance(xA, yA, zA, xB, yB, zB));
